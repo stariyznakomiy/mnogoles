326e7318e78e5ab77caef3488265ba032dd08a25
davidenko.sinergium.ru <stariyznakomiy0@gmail.com>
2025-05-24 01:43:02 +0300

    Верстает каталог

--- dist/js/app.js
+++ dist/js/app.js
@@ -4087,6 +4087,13 @@
                 document.documentElement.classList.add(className);
             }));
         }
+        function functions_getHash() {
+            if (location.hash) return location.hash.replace("#", "");
+        }
+        function setHash(hash) {
+            hash = hash ? `#${hash}` : window.location.href.split("#")[0];
+            history.pushState("", "", hash);
+        }
         let _slideUp = (target, duration = 500, showmore = 0) => {
             if (!target.classList.contains("_slide")) {
                 target.classList.add("_slide");
@@ -4272,6 +4279,103 @@
                         }
                     }));
                 }));
+            }
+        }
+        function tabs() {
+            const tabs = document.querySelectorAll("[data-tabs]");
+            let tabsActiveHash = [];
+            if (tabs.length > 0) {
+                const hash = functions_getHash();
+                if (hash && hash.startsWith("tab-")) tabsActiveHash = hash.replace("tab-", "").split("-");
+                tabs.forEach(((tabsBlock, index) => {
+                    tabsBlock.classList.add("_tab-init");
+                    tabsBlock.setAttribute("data-tabs-index", index);
+                    tabsBlock.addEventListener("click", setTabsAction);
+                    initTabs(tabsBlock);
+                }));
+                let mdQueriesArray = dataMediaQueries(tabs, "tabs");
+                if (mdQueriesArray && mdQueriesArray.length) mdQueriesArray.forEach((mdQueriesItem => {
+                    mdQueriesItem.matchMedia.addEventListener("change", (function() {
+                        setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+                    }));
+                    setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+                }));
+            }
+            function setTitlePosition(tabsMediaArray, matchMedia) {
+                tabsMediaArray.forEach((tabsMediaItem => {
+                    tabsMediaItem = tabsMediaItem.item;
+                    let tabsTitles = tabsMediaItem.querySelector("[data-tabs-titles]");
+                    let tabsTitleItems = tabsMediaItem.querySelectorAll("[data-tabs-title]");
+                    let tabsContent = tabsMediaItem.querySelector("[data-tabs-body]");
+                    let tabsContentItems = tabsMediaItem.querySelectorAll("[data-tabs-item]");
+                    tabsTitleItems = Array.from(tabsTitleItems).filter((item => item.closest("[data-tabs]") === tabsMediaItem));
+                    tabsContentItems = Array.from(tabsContentItems).filter((item => item.closest("[data-tabs]") === tabsMediaItem));
+                    tabsContentItems.forEach(((tabsContentItem, index) => {
+                        if (matchMedia.matches) {
+                            tabsContent.append(tabsTitleItems[index]);
+                            tabsContent.append(tabsContentItem);
+                            tabsMediaItem.classList.add("_tab-spoller");
+                        } else {
+                            tabsTitles.append(tabsTitleItems[index]);
+                            tabsMediaItem.classList.remove("_tab-spoller");
+                        }
+                    }));
+                }));
+            }
+            function initTabs(tabsBlock) {
+                let tabsTitles = tabsBlock.querySelectorAll("[data-tabs-titles]>*");
+                let tabsContent = tabsBlock.querySelectorAll("[data-tabs-body]>*");
+                const tabsBlockIndex = tabsBlock.dataset.tabsIndex;
+                const tabsActiveHashBlock = tabsActiveHash[0] == tabsBlockIndex;
+                if (tabsActiveHashBlock) {
+                    const tabsActiveTitle = tabsBlock.querySelector("[data-tabs-titles]>._tab-active");
+                    tabsActiveTitle ? tabsActiveTitle.classList.remove("_tab-active") : null;
+                }
+                if (tabsContent.length) {
+                    tabsContent = Array.from(tabsContent).filter((item => item.closest("[data-tabs]") === tabsBlock));
+                    tabsTitles = Array.from(tabsTitles).filter((item => item.closest("[data-tabs]") === tabsBlock));
+                    tabsContent.forEach(((tabsContentItem, index) => {
+                        tabsTitles[index].setAttribute("data-tabs-title", "");
+                        tabsContentItem.setAttribute("data-tabs-item", "");
+                        if (tabsActiveHashBlock && index == tabsActiveHash[1]) tabsTitles[index].classList.add("_tab-active");
+                        tabsContentItem.hidden = !tabsTitles[index].classList.contains("_tab-active");
+                    }));
+                }
+            }
+            function setTabsStatus(tabsBlock) {
+                let tabsTitles = tabsBlock.querySelectorAll("[data-tabs-title]");
+                let tabsContent = tabsBlock.querySelectorAll("[data-tabs-item]");
+                const tabsBlockIndex = tabsBlock.dataset.tabsIndex;
+                function isTabsAnamate(tabsBlock) {
+                    if (tabsBlock.hasAttribute("data-tabs-animate")) return tabsBlock.dataset.tabsAnimate > 0 ? Number(tabsBlock.dataset.tabsAnimate) : 500;
+                }
+                const tabsBlockAnimate = isTabsAnamate(tabsBlock);
+                if (tabsContent.length > 0) {
+                    const isHash = tabsBlock.hasAttribute("data-tabs-hash");
+                    tabsContent = Array.from(tabsContent).filter((item => item.closest("[data-tabs]") === tabsBlock));
+                    tabsTitles = Array.from(tabsTitles).filter((item => item.closest("[data-tabs]") === tabsBlock));
+                    tabsContent.forEach(((tabsContentItem, index) => {
+                        if (tabsTitles[index].classList.contains("_tab-active")) {
+                            if (tabsBlockAnimate) _slideDown(tabsContentItem, tabsBlockAnimate); else tabsContentItem.hidden = false;
+                            if (isHash && !tabsContentItem.closest(".popup")) setHash(`tab-${tabsBlockIndex}-${index}`);
+                        } else if (tabsBlockAnimate) _slideUp(tabsContentItem, tabsBlockAnimate); else tabsContentItem.hidden = true;
+                    }));
+                }
+            }
+            function setTabsAction(e) {
+                const el = e.target;
+                if (el.closest("[data-tabs-title]")) {
+                    const tabTitle = el.closest("[data-tabs-title]");
+                    const tabsBlock = tabTitle.closest("[data-tabs]");
+                    if (!tabTitle.classList.contains("_tab-active") && !tabsBlock.querySelector("._slide")) {
+                        let tabActiveTitle = tabsBlock.querySelectorAll("[data-tabs-title]._tab-active");
+                        tabActiveTitle.length ? tabActiveTitle = Array.from(tabActiveTitle).filter((item => item.closest("[data-tabs]") === tabsBlock)) : null;
+                        tabActiveTitle.length ? tabActiveTitle[0].classList.remove("_tab-active") : null;
+                        tabTitle.classList.add("_tab-active");
+                        setTabsStatus(tabsBlock);
+                    }
+                    e.preventDefault();
+                }
             }
         }
         function menuInit() {
@@ -10694,11 +10798,66 @@
                 const wrapper = el.closest('[data-js="menu-search-row"]');
                 wrapper.classList.toggle("active");
             }
+            if (el.closest('[data-js="sidebar-catalog"]')) {
+                const wrapper = document.querySelector('[data-js="sidebar"]');
+                if (wrapper) {
+                    wrapper.classList.add("active");
+                    wrapper.classList.add("_catalog");
+                    bodyLock();
+                }
+            }
+            if (el.closest('[data-js="sidebar-filters"]')) {
+                const wrapper = document.querySelector('[data-js="sidebar"]');
+                if (wrapper) {
+                    wrapper.classList.add("active");
+                    wrapper.classList.add("_filters");
+                    bodyLock();
+                }
+            }
+            if (el.closest('[data-js="sidebar-close"]')) {
+                const wrapper = document.querySelector('[data-js="sidebar"]');
+                if (wrapper) {
+                    wrapper.classList.remove("active");
+                    wrapper.classList.remove("_catalog");
+                    wrapper.classList.remove("_filters");
+                }
+                if (bodyLockStatus) bodyUnlock();
+            }
+        }
+        let map = document.querySelector("#map");
+        if (map) {
+            ymaps.ready(init);
+            function init() {
+                const map = new ymaps.Map("map", {
+                    center: [ 55.63427283250651, 37.440162013813115 ],
+                    zoom: 17
+                });
+                const BalloonLayout = ymaps.templateLayoutFactory.createClass('<div class="map__balloon">$[properties.balloonContent]</div>');
+                const placemark = new ymaps.Placemark([ 55.63427283250651, 37.440162013813115 ], {
+                    balloonContent: "МногоЛеса<br> Наша торговая точка"
+                }, {
+                    preset: "islands#blueDotIcon",
+                    iconColor: "#1E90FF",
+                    iconImageSize: [ 40, 40 ],
+                    iconImageOffset: [ -20, -20 ],
+                    hideIconOnBalloonOpen: false,
+                    balloonCloseButton: false,
+                    hideHintOnTouch: true,
+                    hintContent: null,
+                    balloonLayout: BalloonLayout,
+                    balloonPanelMaxMapArea: 0,
+                    interactivityModel: "default#silent"
+                });
+                map.geoObjects.add(placemark);
+                placemark.balloon.open();
+                map.controls.remove("geolocationControl").remove("searchControl").remove("trafficControl").remove("typeSelector").remove("fullscreenControl").remove("rulerControl");
+            }
         }
         window["FLS"] = true;
         isWebp();
         menuInit();
         spollers();
+        tabs();
         formSubmit();
     })();
 })();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,5 +1,5 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+import { isMobile, bodyLock, bodyUnlock, bodyLockStatus } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
@@ -11,5 +11,86 @@ function documentActionsClick(e) {
    if (el.closest('[data-js="menu-search-icon"]')) {
       const wrapper = el.closest('[data-js="menu-search-row"]');
       wrapper.classList.toggle('active');
+   }
+
+   if (el.closest('[data-js="sidebar-catalog"]')) {
+      const wrapper = document.querySelector('[data-js="sidebar"]');
+      if (wrapper) {
+         wrapper.classList.add('active');
+         wrapper.classList.add('_catalog');
+         bodyLock();
+      }
+   }
+
+   if (el.closest('[data-js="sidebar-filters"]')) {
+      const wrapper = document.querySelector('[data-js="sidebar"]');
+      if (wrapper) {
+         wrapper.classList.add('active');
+         wrapper.classList.add('_filters');
+         bodyLock();
+      }
+   }
+
+   if (el.closest('[data-js="sidebar-close"]')) {
+      const wrapper = document.querySelector('[data-js="sidebar"]');
+      if (wrapper) {
+         wrapper.classList.remove('active');
+         wrapper.classList.remove('_catalog');
+         wrapper.classList.remove('_filters');
+      }
+      if (bodyLockStatus) {
+         bodyUnlock();
+      }
+   }
+}
+
+let map = document.querySelector('#map');
+
+if (map) {
+   // Инициализация карты после загрузки API
+   ymaps.ready(init);
+
+   function init() {
+      // Создание карты
+      const map = new ymaps.Map("map", {
+         center: [55.63427283250651, 37.440162013813115], // 
+         zoom: 17, // Уровень масштабирования
+      });
+
+      // Кастомный макет балуна
+      const BalloonLayout = ymaps.templateLayoutFactory.createClass(
+         '<div class="map__balloon">$[properties.balloonContent]</div>'
+      );
+
+      // Добавление метки
+      const placemark = new ymaps.Placemark([55.63427283250651, 37.440162013813115], {
+         balloonContent: 'МногоЛеса<br> Наша торговая точка'
+      }, {
+         preset: 'islands#blueDotIcon', // Иконка в виде синей точки с хвостиком
+         iconColor: '#1E90FF',
+         iconImageSize: [40, 40],
+         iconImageOffset: [-20, -20],
+         hideIconOnBalloonOpen: false,
+         balloonCloseButton: false,
+         hideHintOnTouch: true,
+         hintContent: null,
+         balloonLayout: BalloonLayout,
+         balloonPanelMaxMapArea: 0,
+         interactivityModel: 'default#silent',
+      });
+
+      map.geoObjects.add(placemark);
+
+      // Программно открываем балун (без возможности закрыть)
+      placemark.balloon.open();
+
+      // Дополнительные элементы управления
+      map.controls
+         .remove('geolocationControl') // Удаляем геолокацию
+         .remove('searchControl')     // Удаляем поиск
+         .remove('trafficControl')     // Удаляем контроль трафика
+         .remove('typeSelector')       // Удаляем тип карты
+         .remove('fullscreenControl')  // Удаляем кнопку полноэкранного режима
+         .remove('rulerControl');     // Удаляем контрол правил
    }
 }
